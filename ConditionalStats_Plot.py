Filtering with 2D Savitsky Golay. Fixed gradient and velocity direction extraction with flipping

ConditionalStats_Plot.py
@@ -21,12 +21,12 @@ class ConditionalStats_Plot:
         #strng = "TurbulenceStatistics_DP_baseline_otsuby2_velmagsqrt_shearlayeranglemodify_overlayangleadjust_10imgs"
         try:
             #strng = "TurbulenceStatistics_DP_baseline_otsuby4_gradientcalctest_200imgs_withvoriticity_interfacecheck"
-            strng = "TurbulenceStatistics_DP_baseline_otsuby8_gradientcalctest_20imgs_withvoriticity_interfacecheck_fixeddirectionality"
+            strng = "TurbulenceStatistics_DP_baseline_otsuby4_gradientcalctest_100imgs_withvoriticity_interfacecheck_fixeddirectionality_spatialfreq2"
             file_path2 = loc + strng + '.pkl'
             with open(file_path2,'rb') as f:
                 mat = pickle.load(f)
         except:
-            strng = "TurbulenceStatistics_DP_baseline_otsuby2_gradientcalctest_10imgs_withvoriticity_interfacecheck"
+            strng = "TurbulenceStatistics_DP_baseline_otsuby1.5_gradientcalctest_100imgs_withvoriticity_interfacecheck_fixeddirectionality_spatialfreq2"
             file_path2 = loc + strng + '.pkl'
             with open(file_path2, 'rb') as f:
                 mat = pickle.load(f)
@@ -72,9 +72,9 @@ class ConditionalStats_Plot:
                     680:"O:/JetinCoflow/15D_680rpm/"}
         leg_dict={0: 0, 375: 0.16, 680 : 0.33}
         u_coflow_dict={0: 0, 375: 3.1953, 680: 6.6}
-        key_list = [0]#,375,680]#,375]
-        xloc = [150]#, 100, 400, 550]  # self.DP.X_pos
-        h_win = 50  # +/- hwin
+        key_list = [0,375,680]#,375,680]#,375]
+        xloc = [100]#,,680 100, 400, 550]  # self.DP.X_pos
+        h_win = 20  # +/- hwin
         """fig, ax = plt.subplots()
         img = ax.imshow(np.mean(self.DP.layer_U, axis=2)[:, :, 0])
         fig.colorbar(img)"""
@@ -131,6 +131,7 @@ class ConditionalStats_Plot:
             if vorticity_plot_opt == 'y':
                 K_td, K_t, K_nu, K_nu_t, K_adv,enstrophy,vorticity, vorticity_mod, enstrophy_flux = KE.ke_budget_terms(mean_u_cond, mean_v_cond, uprime_cond, vprime_cond, dx,
                                                                 dy,self.DP.layer_U,self.DP.layer_V,self.DP.layer_omega[:,:,:,0])
+                #K_td, K_t, K_nu, K_nu_t, K_adv, omega, Omega_mean, Omeage_modulus_mean, enstrophy_flux
             mrkr_size = 10
             for i in range(len(xloc)):
                 ind = xloc[i]
@@ -175,10 +176,10 @@ class ConditionalStats_Plot:
                     vorticity_plot = np.mean(vorticity[:, start_ind:stop_ind], axis=1)
                     vorticity_mod_plot = np.mean(vorticity_mod[:, start_ind:stop_ind], axis=1)
                     enstrophy_flux_plot = np.mean(enstrophy_flux[:, start_ind:stop_ind], axis=1)
-                denom_fact = (Ucenter)# - self.u_coflow)
-                denom_fact_ke = (Ucenter) ** 3.0#(Ucenter - self.u_coflow) ** 3.0
+                denom_fact = (Ucenter - self.u_coflow)
+                denom_fact_ke = (Ucenter - self.u_coflow) ** 3.0#(Ucenter) ** 3.0#
 
-                ax.scatter(xplot, (Uplot) / denom_fact,
+                ax.scatter(xplot, (Uplot- self.u_coflow) / denom_fact,
                            s=mrkr_size)  # np.linspace(0,len(Uplot)-1,len(Uplot))
                 ax.set_ylabel('U/U$_c$')
                 ax.set_xlabel('r/D')
@@ -225,8 +226,8 @@ class ConditionalStats_Plot:
                     ax10.set_ylabel('Enstrophy')
                     ax10.set_xlabel('r/D')
 
-                    ax11.scatter(xplot, vorticity_plot, s=mrkr_size)
-                    ax11.set_ylabel('Vorticity')
+                    ax11.scatter(xplot, -vorticity_plot, s=mrkr_size)
+                    ax11.set_ylabel('-Vorticity')
                     ax11.set_xlabel('r/D')
 
                     ax12.scatter(xplot, vorticity_mod_plot, s=mrkr_size)

InterfaceDetection.py
@@ -5,6 +5,7 @@ import Settings
 import warnings
 import math
 from scipy.interpolate import LinearNDInterpolator,interpn
+from SavitskyGolay2D import sgolay2d
 
 class InterfaceDetection:
     def __init__(self, U, num, num_pos):
@@ -16,12 +17,31 @@ class InterfaceDetection:
         self.shear_num = num
         self.num_pos = num_pos
 
+
     def Detect(self, u, v, x, y, layer):
-        plot_img='n'
+        plot_img='y'
         edge = EdgeDetect.Edge()
+        u_sg = sgolay2d(u, window_size=5, order=2)
+        v_sg = sgolay2d(v, window_size=5, order=2)
+        u_derivy, u_derivx = sgolay2d(u, window_size=5, order=2,derivative='both')
+        v_derivy, v_derivx = sgolay2d(v, window_size=5, order=2,derivative='both')
+        omega = v_derivx-u_derivy
+        u = u_sg
+        v= v_sg
         X_I, Y_I, img_proc0,dx,dy,x0,y0, contours, cluster_img = edge.data_detect(u,v,x,y)
         print("Y_I shape=",np.shape(Y_I))
         m,X_out,Y_out,grad2 = self.edge_slope((X_I-x0)/dx,(Y_I-y0)/dy,contours,dx,dy,x0,y0)
+        # Todo: Extract gradient after smoothing velocity field
+        """dvdx = np.diff(v, axis=1)
+        dvdx = np.insert(dvdx, 0, dvdx[:, -1], axis=1)
+        dudy = np.diff(u, axis=0)
+        dudy = np.insert(dudy, -1, dudy[-1, :], axis=0)
+        omega = dvdx - dudy"""
+        # plt.yscale('log')
+        """plt.subplots()
+        plt.imshow(omega)
+        plt.show()"""
+
         """x_unq = np.unique(X_I)
         y_unq = np.array([])
         for i in range(len(x_unq)):
@@ -36,9 +56,15 @@ class InterfaceDetection:
             plt.scatter((X_out - x0) / dx, (Y_out - y0) / dy, c='k', s=5)
             plt.colorbar
 
+            plt.subplots()
+            plt.imshow(u_sg)
+            plt.colorbar
+
         #plt.show()
         #self.plot_interface(v, y[:, 0], X_I, Y_I)
-        self.ShearLayer(X_out, Y_out,m,grad2,x, y, u, v,dx,dy,x0,y0,cluster_img)
+        dx_cond = dx/2.0
+        dy_cond = dy/2.0
+        self.ShearLayer(X_out, Y_out,m,grad2,x, y, u, v,dx_cond,dy_cond,x0,y0,cluster_img,omega,u_derivy,u_derivx,v_derivy, v_derivx)
         print('Max u=',np.max(u))
 
         if plot_img=='y':
@@ -51,7 +77,7 @@ class InterfaceDetection:
             off_side = np.where(side_arr[:, 0] > 0.75)[0]
             color_arr[off_side] = np.flip(color_arr[off_side], 1)"""
             plt.subplots()
-            #plt.imshow(img_proc0)
+            plt.imshow(img_proc0)
             sc = plt.scatter(xlayer_plot, ylayer_plot, c= color_arr,cmap='jet', s=9)
             #sc = plt.scatter((self.layer_x[::1, :, 0] - x0) / dx, (self.layer_y[::1, :, 0] - y0) / dy,c=self.side_val[::1, :, 0], cmap='jet', s=9)
             #plt.scatter(self.layer_x[31, :], self.layer_y[31, :], c='r', s=5)
@@ -68,6 +94,13 @@ class InterfaceDetection:
             plt.colorbar(sc)
 
             plt.show()
+
+    def interpolate_array(self,points,omega,interp_pts,shp_cast):
+        omega_val = interpn(points, np.transpose(omega), interp_pts)
+        omega_val = np.stack(omega_val)
+        omega_val = np.reshape(omega_val, shp_cast)
+
+        return omega_val
 
     def edge_slope(self, X,Y,contours,dx,dy,x00,y00):
         """
@@ -344,7 +377,7 @@ class InterfaceDetection:
 
         return x2, y2
 
-    def ShearLayer(self, X_I, Y_I,m,grad2,x, y, u, v,dx,dy,x0,y0,cluster_img):
+    def ShearLayer(self, X_I, Y_I,m,grad2,x, y, u, v,dx,dy,x0,y0,cluster_img,omega,u_derivy,u_derivx,v_derivy, v_derivx):
         edge_length = len(Y_I) #len(x[0,:])
         Shear_layer = np.zeros((self.shear_num, edge_length, 5,7))
         self.skipped_lines_sub = []
@@ -369,11 +402,7 @@ class InterfaceDetection:
         plt.scatter(X_I,Y_I)"""
         edge_instance = np.zeros(edge_length)
         x_ind_rec=[]
-        dvdx = np.diff(v, axis=1)
-        dvdx = np.insert(dvdx, 0, dvdx[:, -1], axis=1)
-        dudy = np.diff(u, axis=0)
-        dudy = np.insert(dudy, -1, dudy[-1, :], axis=0)
-        omega = dvdx - dudy
+
         x_search = np.array(x)
         y_search = np.array(y)
         Shear_layer_temp = np.zeros((self.shear_num, 5, 7))
@@ -412,36 +441,41 @@ class InterfaceDetection:
         #interp_pts = list(zip(val[:, 0, :, :], val[:, 1, :, :]))
         points =(x[0,:],y[:,0])
         shp_cast = (shp_val[0],shp_val[1],shp_val[2])
-        u_val = interpn(points,np.transpose(u),interp_pts)
-        u_val = np.stack(u_val)
-        u_val=np.reshape(u_val,shp_cast)
-        v_val = interpn(points, np.transpose(v), interp_pts)
-        v_val = np.stack(v_val)
-        v_val = np.reshape(v_val, shp_cast)
-        omega_val = interpn(points, np.transpose(omega), interp_pts)
-        omega_val = np.stack(omega_val)
-        omega_val = np.reshape(omega_val, shp_cast)
-        side_val = interpn(points, np.transpose(cluster_img), interp_pts)
-        side_val = np.stack(side_val)
-        side_val = np.reshape(side_val, shp_cast)
+        u_val = self.interpolate_array(points, u, interp_pts, shp_cast)
+        v_val = self.interpolate_array(points, v, interp_pts, shp_cast)
+        omega_val = self.interpolate_array(points, omega, interp_pts, shp_cast)
+        uderivx_val = self.interpolate_array(points, u_derivx, interp_pts, shp_cast)
+        uderivy_val = self.interpolate_array(points, u_derivy, interp_pts, shp_cast)
+        vderivx_val = self.interpolate_array(points, v_derivx, interp_pts, shp_cast)
+        vderivy_val = self.interpolate_array(points, v_derivy, interp_pts, shp_cast)
+        side_val = self.interpolate_array(points, cluster_img, interp_pts, shp_cast)
         x_val_interp = np.reshape(x_val_interp, shp_cast)
         y_val_interp = np.reshape(y_val_interp, shp_cast)
         print("Ylayer length=",len(Y_I))
         print("layer shape=",np.shape(x_val_interp))
-         #Inversion based on which side of the curve the starting point lies on
+        # Inversion based on which side of the curve the starting point lies on
         side_arr = np.array(side_val[:, :, 0])
         off_side = np.where(side_arr[:,0] <0.5)[0]
         x_val_interp[off_side] = np.flip(x_val_interp[off_side], 1)
         y_val_interp[off_side] = np.flip(y_val_interp[off_side], 1)
         u_val[off_side] = np.flip(u_val[off_side], 1)
         v_val[off_side] = np.flip(v_val[off_side], 1)
-        omega_val[off_side] = np.flip(omega_val[off_side], 0)
+        omega_val[off_side] = np.flip(omega_val[off_side], 1)
+        uderivx_val[off_side] = np.flip(uderivx_val[off_side], 1)
+        uderivy_val[off_side] = np.flip(uderivy_val[off_side], 1)
+        vderivx_val[off_side] = np.flip(vderivx_val[off_side], 1)
+        vderivy_val[off_side] = np.flip(vderivy_val[off_side], 1)
+        # Final output, swap axis= 0 and 1
         shp_cast = (shp_val[0],shp_val[1],shp_val[2])
         self.layer_x = np.swapaxes(x_val_interp, 0,1)
         self.layer_y = np.swapaxes(y_val_interp, 0,1)
         self.layer_U = np.swapaxes(u_val, 0,1)
         self.layer_V = np.swapaxes(v_val, 0,1)
         self.layer_omega= np.swapaxes(omega_val, 0,1)
+        self.layer_uderivx = np.swapaxes(uderivx_val, 0, 1)
+        self.layer_uderivy = np.swapaxes(uderivy_val, 0, 1)
+        self.layer_vderivx = np.swapaxes(vderivx_val, 0, 1)
+        self.layer_vderivy = np.swapaxes(vderivy_val, 0, 1)
         self.side_val = np.swapaxes(side_val, 0,1)
         self.Y_plot = np.tile(np.arange(1, self.shear_num + 1), (1, self.num_pos)) - (self.shear_num / 2)
 

KineticEnergy.py
@@ -184,7 +184,7 @@ def ke_budget_terms(U,V,uprime,vprime,dx,dy,U_inst,V_inst,omega_inst):
 
     dUdx, dUdy, dUdz, d2Udx2, d2Udy2, d2Udz2, d2Udxdy, d2Udxdz, d2Udydz = derivatives_inst(U_inst, dx, dy)
     dVdx, dVdy, dVdz, d2Vdx2, d2Vdy2, d2Vdz2, d2Vdxdy, d2Vdxdz, d2Vdydz = derivatives_inst(V_inst, dx, dy)
-    Omega_mean = np.mean(omega_inst,axis=2)
+    Omega_mean = np.mean((omega_inst),axis=2)
     Omeage_modulus_mean = np.mean(np.abs(dVdx - dUdy),axis=2)
     # Subtraction by Broadcasting. Taking transpose becomes essential to subtract a mean matrix from a 3D dataset.
     omega = np.transpose(omega_inst)#(dVdx-dUdy)

Run.py
@@ -14,15 +14,16 @@ class Run:
         settings = Settings.Settings()
         DP = DataProcessingConditional.DataProcessor_Conditional()
         #header = np.array(["O:/JetinCoflow/rpm0_ax15D_centerline_dt35_1000_vloc1_1mmsheet_fstop4_PIV_MP(2x24x24_75ov)_5000imgs_20D=unknown/"])
-        #header = np.array(["O:/JetinCoflow/15D_375rpm/"])
-        header = np.array(["O:/JetinCoflow/15D_680rpm/"])
+        header = np.array(["O:/JetinCoflow/15D_375rpm/"])
+        #header = np.array(["O:/JetinCoflow/15D_680rpm/"])
+        #header=np.array(["H:/HeatedFlow_P60_phi080_fstop11_dt6.5_f1v1SeqPIV_MPd(3x24x24_75ov_ImgCorr)=unknown/"])
         h0=header[0]
         DP.processor(settings,header)
         DA = DataAccess.DataAccess();
         #KE = KE_budget.KE_budget();
         #KE.budget(DP.U, DP.V, DP.dx, DP.dx, DP.u_rms, DP.v_rms, DP.uv_mean,settings.nu, DP.yval2, DP.xval2, DP.tke_proc, DP.tke)
         loc = DA.header_def(header[0])
-        strng = "TurbulenceStatistics_DP_baseline_otsuby8_gradientcalctest_20imgs_withvoriticity_interfacecheck_fixeddirectionality"#otsuby1_velmagsqrt_shearlayeranglemodify_overlayangleadjust
+        strng = "TurbulenceStatistics_DP_baseline_otsuby4_gradientcalctest_100imgs_withvoriticity_interfacecheck_fixeddirectionality_spatialfreq2"#otsuby1_velmagsqrt_shearlayeranglemodify_overlayangleadjust
 
 
         # Assuming `loc` is the directory and `strng` is the file name